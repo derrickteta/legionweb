f0532456092f6b718c207504cd2960e17ad34d4a
derrick TETA <derrickteta1@gmail.com>
2021-03-23 01:36:41 +0100

    deuxieme commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { EquipeComponent } from './pages/equipe/equipe.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ClientsComponent } from './pages/clients/clients.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"", component: AccueilComponent
+  },
+  {
+    path:"contact" , component: ContactComponent
+  },
+  {
+    path:"equipe", component: EquipeComponent
+  },
+  {
+    path:"services", component:ServicesComponent
+  },
+  {
+    path:"clients", component:ClientsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { ClientsComponent } from './pages/clients/clients.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { EquipeComponent } from './pages/equipe/equipe.component';
+import { ServicesComponent } from './pages/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    AccueilComponent,
+    ClientsComponent,
+    ContactComponent,
+    EquipeComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
