0fba1275e3dd5981a7d546010f17a5dfe48e4515
derrick TETA <derrickteta1@gmail.com>
2021-06-28 13:22:55 +0100

    vf2

--- legionweb-master/src/app/components/discuter/discuter.component.ts
+++ legionweb-master/src/app/components/discuter/discuter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-discuter',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscuterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router) { }
 
   ngOnInit(): void {
   }
-
+    redirect(r:string){
+    this.router.navigate(['./'+r]);
+  }
 }
