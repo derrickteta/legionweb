e82644aafcf48a8ee41c8270414f57401278844f
derrick TETA <derrickteta1@gmail.com>
2021-07-11 21:18:25 +0100

    blog géré

--- legionweb-master/src/app/components/header/header.component.ts
+++ legionweb-master/src/app/components/header/header.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  public aria:boolean | undefined;
   constructor() { }
-
-  ngOnInit(): void {
-  }  
   
+  ngOnInit(): void {
+    if(screen.width<992){
+      this.aria=false;
+    }
+    else{
+      this.aria=true;
+    }
+  }  
+  derouler (){
+    if(screen.width<992){
+        if(this.aria==true){
+          this.aria=false;
+        }
+        else{
+          this.aria=true;
+        }
+    }
+  }
 
 }
